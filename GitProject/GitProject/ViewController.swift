41e7cf78978eeb5ec51bd052c58017e14b0d600b
SachinAlyx <42204414+SachinAlyx@users.noreply.github.com>
2018-08-08 15:23:38 +0530

    Add files via upload

---
+++ GitProject/GitProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GitProject
+//
+//  Created by sachin on 08/08/18.
+//  Copyright © 2018 Alyx Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       print("hello Swift!!!")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
